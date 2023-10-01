4710d3a2ad085522d28572c7eace2f2e181cce58
b1nd1901 <b1nd@tlen.pl>
2023-10-01 18:16:19 +0200

    Initial commit

---
+++ Udemy_course.py
@@ -0,0 +1,98 @@
+from datetime import datetime
+countries_done = []
+
+
+def read():
+    with open("data.tdo", 'r') as store_file_def:
+        countries_def = store_file_def.readlines()
+    return countries_def
+
+
+
+def write():
+    with open('data.tdo', 'w') as store_file_def:
+        store_file_def.writelines(countries)
+
+
+def pusta():
+    print('Lista jest pusta !')
+
+
+def data():
+    datadzis = datetime.now().strftime("%d / %m / %y")
+    return datadzis
+
+
+while True:
+
+    try:
+        with open("data.tdo", 'r') as store_file:
+            countries = store_file.readlines()
+
+    except FileNotFoundError:  # This is skipped if file exists
+        with open('data.tdo', 'w') as store_file:
+            pass
+    else:
+        break
+
+
+while True:
+    print('Co chcesz zrobić?  ')
+    print('---------------')
+    print("1. Dodaj do listy")
+    print("2. Zaznacz jako zakończone")
+    print("3. Wyświetl listę")
+    print("4. Edytuj wpis")
+    print("5. Wyjdź")
+    print("---------------")
+    print('\n')
+    usr = input("Podaj:")
+
+# Dodaj
+    if '1' in usr[0]:
+        usr_input = usr.strip('1 ')
+        if len(usr_input) > 1:
+            # data = data()
+            countries.append(data() + " " + usr_input + '\n')
+            write()
+        else:
+            print('Nic do dodania')
+
+# Zaznacz jako zakończone
+    elif '2' in usr[0]:
+        try:
+            if len(countries) > 0:
+                countries_done.append(countries.pop(int(input("Który element chcesz zaznaczyć jako ukończony?:"))-1))
+                write()
+
+            else:
+                pusta()
+        except ValueError:
+            print('Nie podałeś prawidłowej wartości.')
+# Wyświetl listę
+    elif '3' in usr[0]:
+        read()
+        if len(countries) > 0:
+            for index, x in enumerate(countries):
+                print(f"{index+1}. {x.strip()}")
+        else:
+            pusta()
+
+# Edytuj wpis
+    elif '4' in usr[0]:
+        if len(countries) > 0 and len(usr[2:]) > 0:
+            usr = int(usr[2:])-1
+            countries[usr] = data() + str(" " + input('Podaj nową wartość:\n') + '\n')
+            write()
+
+        else:
+            if len(usr[2:]) > 0:
+                pusta()
+            else:
+                print('Nie podałeś który element do edycji')
+
+# exit
+    elif '5' in usr[0]:
+        break
+    else:
+        print('Niepoprawny wybór')
---
+++ test.py
@@ -0,0 +1,6 @@
+import random
+
+lower_digit = int(input('Od:'))
+upper_digit = int(input('Do:'))
+
+print(random.randint(lower_digit, upper_digit))
